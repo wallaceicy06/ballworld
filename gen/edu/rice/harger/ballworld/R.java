9249c6f5d1890be3faad3df28e9d5f8130ae3aa0
Sean Harger <seanharger@me.com>
2013-12-01 01:28:00 -0600

    Added test dialog for create new ball action button. It's the missile example from the Android Developer examples

--- gen/edu/rice/harger/ballworld/R.java
+++ gen/edu/rice/harger/ballworld/R.java
@@ -39,6 +39,9 @@ public final class R {
         public static final int action_newball=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int cancel=0x7f050006;
+        public static final int dialog_fire_missiles=0x7f050004;
+        public static final int fire=0x7f050005;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
--- src/edu/rice/harger/ballworld/MainActivity.java
+++ src/edu/rice/harger/ballworld/MainActivity.java
@@ -2,48 +2,66 @@ package edu.rice.harger.ballworld;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
 public class MainActivity extends Activity implements OnTouchListener {
 
-	private BallPitView myBallPitView;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		myBallPitView = new BallPitView(this);
-		myBallPitView.setOnTouchListener(this);
-	
-		setContentView(myBallPitView);
-	}
+    private BallPitView myBallPitView;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		switch(event.getAction()) {
-			case MotionEvent.ACTION_UP:
-				break;
-			
-			case MotionEvent.ACTION_DOWN:
-				myBallPitView.selectObject(new Point(event.getX(), event.getY()));
-				break;
-			
-			case MotionEvent.ACTION_MOVE: 
-				System.out.println("Action move");
-				myBallPitView.moveObject(new Point(event.getX(), event.getY()));
-				break;
-		}
-		return true;
-	}
+        myBallPitView = new BallPitView(this);
+        myBallPitView.setOnTouchListener(this);
+
+        setContentView(myBallPitView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.action_newball:
+            System.out.println("new ball selected");
+            DialogFragment newBallFragment = new NewBallDialogFragment();
+            newBallFragment.show(getFragmentManager(), "missiles");
+            return true;
+        case R.id.action_settings:
+            System.out.println("settings selected");
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+        case MotionEvent.ACTION_UP:
+            break;
+
+        case MotionEvent.ACTION_DOWN:
+            myBallPitView.selectObject(new Point(event.getX(), event.getY()));
+            break;
+
+        case MotionEvent.ACTION_MOVE:
+            System.out.println("Action move");
+            myBallPitView.moveObject(new Point(event.getX(), event.getY()));
+            break;
+        }
+        return true;
+    }
 
 }
